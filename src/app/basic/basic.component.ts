1f2cb974f60e0445fc5c2a8f007b9d342cdc74db
Roman K <roman.k.person@gmail.com>
2020-09-07 20:48:38 +0300

    adding don't work yet

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import { LESSONS } from 'src/db-data';
 import { GRADES } from 'src/db-data';
 
@@ -7,11 +13,35 @@ import { GRADES } from 'src/db-data';
   templateUrl: './basic.component.html',
   styles: [],
 })
-export class BasicComponent {
+export class BasicComponent implements AfterViewInit {
   LESSONS;
   GRADES;
   constructor() {
     this.LESSONS = LESSONS;
     this.GRADES = GRADES;
+  }
+
+  @ViewChild('nmbr')
+  nmbr: ElementRef;
+
+  @ViewChild('dt')
+  dt: ElementRef;
+
+  @ViewChild('tpc')
+  tpc: ElementRef;
+
+  @ViewChild('hmwrk')
+  hmwrk: ElementRef;
+
+  @ViewChild('nt')
+  nt: ElementRef;
+
+  onClick($event) {
+    console.log(this.nt.nativeElement.value);
+    this.LESSONS.push('this.nmbr', 'this.dt', 'this.tpc', 'this.hmwrk', 'this.nt')
+  }
+
+  ngAfterViewInit() {
+    //console.log(this.nt.nativeElement);
   }
 }
